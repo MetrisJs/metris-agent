c5be944a33b716df574113d010b1e987e53b6f4e
Niroshan Sugirtharatnam <codeniro@gmail.com>
2014-09-21 04:53:43 -0400

    moved random Id generator for requests to Metris Base

---
+++ test/lib/generateIdForRequest.test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var generateIdForRequest = require("../../lib/generateIdForRequest.js");
+
+describe("Library: generateIdForRequest()", function () {
+
+  it("returns a random string that's legible", function () {
+
+  });
+
+});
