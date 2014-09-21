52aafeba097215d130458cd9766a203554ea9718
Niroshan Sugirtharatnam <codeniro@gmail.com>
2014-09-21 08:11:54 -0400

    removed console.log

--- test/lib/generateIdForRequest.test.js
+++ test/lib/generateIdForRequest.test.js
@@ -7,8 +7,6 @@ describe("Library: generateIdForRequest()", function () {
   it("returns a random string", function () {
     var randomId = generateIdForRequest("serverName", "127.0.0.1");
 
-    console.log("randomId", randomId);
-
     expect(randomId).to.be.a.string;
   });
 
